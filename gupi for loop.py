3c578187ada859465dca59a51add4b6397321de1
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-03-18 19:36:40 +0600

    Add files via upload

---
+++ gupi for loop.py
@@ -0,0 +1,11 @@
+print("My name GUPI, I'm  from Mars")
+
+food = int(input("Enter Food KG: "))
+print("Every days I eat 1/2 Kg food from total",food,"After that I eat another 1/2 Kg khabar from bechay thaka food thakay ++ ")
+stay = 0
+for stay in range(food):
+    food /= 2
+    stay += 1
+    if food <= 1:
+        break
+    print("Food",food,"Days",stay)
