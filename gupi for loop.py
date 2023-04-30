21f3d6dae435b622e093ead991e8be004024924b
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-04-30 15:42:31 +0600

    Update gupi for loop.py

--- gupi for loop.py
+++ gupi for loop.py
@@ -1,7 +1,7 @@
 print("My name GUPI, I'm  from Mars")
 
 food = int(input("Enter Food KG: "))
-print("Every days I eat 1/2 Kg food from total",food,"After that I eat another 1/2 Kg khabar from bechay thaka food thakay ++ ")
+print("Every days I eat 1/2 Kg food from total",food,"After that I eat another 1/2 Kg khabar from bechay thaka food thakay -- minus minus ha ha ")
 stay = 0
 for stay in range(food):
     food /= 2
