cb110b0ce9a06983e35fa7b7f5320918c41923b9
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-06-15 13:30:23 +0600

    Create float_magic.py

---
+++ float_magic.py
@@ -0,0 +1,69 @@
+percent: float = .3758
+print(f"After decimal two: {percent:.2f}")
+
+percent: float = .3758
+print(f"After decimal three: {percent:.3f}")
+
+percent: float = .3758
+print(f"{percent:.1%}")
+
+percent: float = .3758
+print(f"{percent:.2%}")
+
+percent: float = .3758
+print(f"{percent:.3%}")
+
+percent: float = .3758
+print(f'{percent * 10}%')
+
+percent: float = .3758
+print(f'{percent * 100}%')
+
+percent: float = 500.3758
+print(f'{percent * 100}%')
+
+percent: float = 500.3758
+print(f'{percent:,.2f}')
+
+percent: float = 500.3758
+print(f'{percent:,.3f}')
+
+percent: float = 500.3758
+print(f'{percent:,.2%}')
+
+percent: float = 500.3758
+print(f'{percent:,.3%}')
+
+percent: float = 50.3758
+print(f"After decimal two: {percent:.2f}")
+
+percent: float = 50.3758
+print(f"After decimal three: {percent:.3f}")
+
+percent: float = 500.3758
+print(f'{percent:.2%}')
+
+percent: float = 500.3758
+print(f'{percent:.3%}')
+
+
+#------------------------------------------------------------------
+
+# Answer: 
+
+# After decimal two: 0.38
+# After decimal three: 0.376
+# 37.6%
+# 37.58%
+# 37.580%
+# 3.758%
+# 37.580000000000005%
+# 50037.58%
+# 500.38
+# 500.376
+# 50,037.58%
+# 50,037.580%
+# After decimal two: 50.38
+# After decimal three: 50.376
+# 50037.58%
+# 50037.580%
