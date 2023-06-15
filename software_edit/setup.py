b4b652bd52f91a1fdfe98da83228adcbd52f64f9
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-06-15 13:34:49 +0600

    Add files via upload

---
+++ software_edit/setup.py
@@ -0,0 +1,21 @@
+import cx_Freeze
+import sys
+import os 
+base = None
+
+if sys.platform == 'win32':
+    base = "Win32GUI"
+
+os.environ['TCL_LIBRARY'] = r"C:\Program Files\Python311\tcl\tcl8.6"
+os.environ['TK_LIBRARY'] = r"C:\Program Files\Python311\tcl\tk8.6"
+
+executables = [cx_Freeze.Executable("game.py", base=base, icon="icon.ico")]
+
+
+cx_Freeze.setup(
+    name = "Digital Clock",
+    options = {"build_exe": {"packages":["tkinter","os"], "include_files":["icon.ico",'tcl86t.dll','tk86t.dll', 'icons2']}},
+    version = "0.01",
+    description = "Tkinter Application",
+    executables = executables
+    )
