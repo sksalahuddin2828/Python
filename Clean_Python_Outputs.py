f983ff8506a81fc38cc6e7e4ea9b1de3b49a6fb4
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-06-17 19:52:49 +0600

    Create Clean_Python_Outputs.py

---
+++ Clean_Python_Outputs.py
@@ -0,0 +1,56 @@
+language1 = "C"
+language2 = "C++"
+language3 = "C#"
+language4 = "Java"
+language5 = "JavaScript"
+language6 = "Python"
+language7 = "Swift"
+
+print(f'{language1} : 1972')
+print(f'{language2} : 1983')
+print(f'{language3} : 2000')
+print(f'{language4} : 1995')
+print(f'{language5} : 1997')
+print(f'{language6} : 1991')
+print(f'{language7} : 2005')
+
+# Answer: -->>
+
+C : 1972
+C++ : 1983
+C# : 2000
+Java : 1995
+JavaScript : 1997
+Python : 1991
+Swift : 2005
+
+
+#--------------------------------------------------
+
+
+language1 = "C"
+language2 = "C++"
+language3 = "C#"
+language4 = "Java"
+language5 = "JavaScript"
+language6 = "Python"
+language7 = "Swift"
+
+print(f'{language1:10} : 1972')
+print(f'{language2:10} : 1983')
+print(f'{language3:10} : 2000')
+print(f'{language4:10} : 1995')
+print(f'{language5:10} : 1997')
+print(f'{language6:10} : 1991')
+print(f'{language7:10} : 2005')
+
+
+# Answer: -->>
+
+C          : 1972
+C++        : 1983
+C#         : 2000
+Java       : 1995
+JavaScript : 1997
+Python     : 1991
+Swift      : 2005
