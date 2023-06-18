8d687933bf37c24d7319db9f9b997a9da4837bd4
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-06-18 18:15:16 +0600

    Update and rename The faster way to Loop in Python to The_faster_way_to_Loop_in_Python.py

--- The_faster_way_to_Loop_in_Python.py
+++ The_faster_way_to_Loop_in_Python.py
@@ -1,17 +1,17 @@
 import timeit
 
-def while_loop(n=100000000):
+def while_loop(n = 100000000):
   i = 0
   s = 0
-  while i<n:
+  while i < n:
     s += 1
     i += 1
   return 0
 
-def for_loop(n=100000000):
+def for_loop(n = 100000000):
   s = 0
   for i in range(n):
-      s+=1
+      s += 1
   return 0
 
 def main():
@@ -19,7 +19,4 @@ def main():
   print("for_loop\t\t", timeit.timeit(for_loop, number=1))
   
 if __name__ == '__main__':
-  main()
-
-
-      
+  main() 
