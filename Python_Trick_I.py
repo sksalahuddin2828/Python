ddfbaad810a96dec6e151a0e71836bb2925efff0
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-06-21 14:40:07 +0600

    Update and rename Python Trick I to Python_Trick_I.py

--- Python_Trick_I.py
+++ Python_Trick_I.py
@@ -1,19 +1,13 @@
 group_of_list = [[1, 2], [3, 4]]
 
 for i in group_of_list:
-
   for element in i:
-
     print(element)
-
 print("")
 
 def function(parameter: str):
-
   print(parameter)
-
   return function 
 
 # You can add here multiple argument 
-
 function('A')('B')('C')
