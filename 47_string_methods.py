9da05dc480248e314b6c83bc91ae7a6763964d0c
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-06-27 14:51:29 +0600

    Create 47_string_methods.py

---
+++ 47_string_methods.py
@@ -0,0 +1,12 @@
+# 1: Capitalize()
+
+_txt: str = "hELlo"
+txt_: str = "heLLO"
+
+print(_txt.capitalize())
+print(txt_.capitalize())
+
+# Answer: Hello
+# Answer: Hello
+
+#-------------------------------------
