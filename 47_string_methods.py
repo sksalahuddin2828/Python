d15b8755aeaa62193818fa08f69c3d878e545c5a
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-06-27 14:52:53 +0600

    Update 47_string_methods.py

--- 47_string_methods.py
+++ 47_string_methods.py
@@ -10,3 +10,17 @@ print(txt_.capitalize())
 # Answer: Hello
 
 #-------------------------------------
+
+# 2: Casefold()
+
+_txt: str = "hELlo"
+txt_: str = "heLLO"
+
+print(_txt.casefold())
+print(txt_.casefold())
+
+# Answer: hello
+# Answer: hello
+
+#-------------------------------------
+
