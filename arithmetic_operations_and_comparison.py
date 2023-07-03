ceadf368b8dfe482ee72c8961d7eeb90cc8478fb
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-03 14:39:12 +0600

    Create arithmetic_operations_and_comparison.py

---
+++ arithmetic_operations_and_comparison.py
@@ -0,0 +1,27 @@
+number_one = 4 
+number_two = 5
+
+summation = number_one + number_two
+subtraction = number_one - number_two
+multiplication = number_one * number_two
+division = number_one / number_two
+modulus = number_one % number_two
+
+print(summation)
+print(subtraction)
+print(multiplication)
+print(division)
+print(modulus)
+
+if number_one == number_two:
+    print ("Equal")
+else:
+    print("Not equal")
+
+
+# Answer: 9
+#        -1
+#        20
+#        0.8
+#        4
+#        Not equal
