76e4b91c361028b0dba34b64cc64d11e2bfb0daa
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-08 13:58:11 +0600

    Update two_dimension_illusion.py

--- 01. illusion in Python/two_dimension_illusion.py
+++ 01. illusion in Python/two_dimension_illusion.py
@@ -54,7 +54,7 @@ class MainWindow(QMainWindow):
         pen.setWidth(2)                                   
 
         self.drawCircle(cx, cy, radius, QColor(0, 0, 0))
-        for i in range(4):  # Adjusted line count         
+        for i in range(4):           
             angle = (i / 4) * 2 * pi                      
             x = cx + radius * cos(angle)
             y = cy + radius * sin(angle)
