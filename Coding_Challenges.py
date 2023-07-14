e3540812349c5bd18bfc03bc39a409fe1fd5bc00
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-14 15:30:39 +0600

    Create Coding_Challenges.py

---
+++ Coding_Challenges.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------Python Coding Challenges---------------------------------------------------------
+
+# Python Coding Challenges on Numbers 
+# Write a program in Python to -
+
+# 1. Convert decimal numbers to octal numbers.
+# 2. Reverse an integer.
+# 3. Print the Fibonacci series using the recursive method.
+# 4. Return the Nth value from the Fibonacci sequence.
+# 5. Find the average of numbers (with explanations).
+# 6. Convert Celsius to Fahrenheit.
+
+#----------------------------------------------------------------Solution of Problem:----------------------------------------------------------
+
+# 1. Converting Decimal Numbers to Octal Numbers:
+
+decimal_number = 25
+octal_number = oct(decimal_number)[2:]
+print(octal_number)  # Output: 31
