8fda7c41bf690d16fb0d968ffc1ebf02b1afa7f8
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 14:08:03 +0600

    Update kaprekar_constant.py

--- kaprekar_constant.py
+++ kaprekar_constant.py
@@ -16,3 +16,21 @@ print(f"Number of steps to reach Kaprekar constant: {steps}")
 
 # Answer: Enter a number: 2352
 #         Number of steps to reach Kaprekar constant: 3
+
+
+#--------------------------------------------------------------------------
+
+def kaprekar_constant(n):
+    count = 0
+    while n != 6174:
+        count += 1
+        digits = str(n).zfill(4)
+        ascending = int(''.join(sorted(digits)))
+        descending = int(''.join(sorted(digits, reverse=True)))
+        n = descending - ascending
+    return count
+
+# Test the function
+print(kaprekar_constant(1234))  
+
+# Output: 3
