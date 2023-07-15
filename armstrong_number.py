c18ae86d474cce03e3808dcfde62db837f9a5ae9
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 14:04:48 +0600

    Update armstrong_number.py

--- armstrong_number.py
+++ armstrong_number.py
@@ -14,3 +14,20 @@ if is_armstrong_number(user_input):
     print(f"{user_input} is an Armstrong number.")
 else:
     print(f"{user_input} is not an Armstrong number.")
+
+#-------------------------------------------------------------------
+
+def is_armstrong_number(n):
+    num_digits = len(str(n))
+    sum_of_powers = 0
+    temp = n
+    while temp > 0:
+        digit = temp % 10
+        sum_of_powers += digit ** num_digits
+        temp //= 10
+    return n == sum_of_powers
+
+# Test the function
+print(is_armstrong_number(153))  
+
+# Output: True
