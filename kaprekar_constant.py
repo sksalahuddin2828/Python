a08609347e15b21dea5f1b91b4b12c531c11865d
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 13:26:56 +0600

    Create kaprekar_constant.py

---
+++ kaprekar_constant.py
@@ -0,0 +1,18 @@
+def kaprekar_constant(n):
+    count = 0
+    while n != 6174:
+        count += 1
+        digits = str(n).zfill(4)
+        ascending = int(''.join(sorted(digits)))
+        descending = int(''.join(sorted(digits, reverse=True)))
+        n = descending - ascending
+    return count
+
+# Prompt user for input
+user_input = int(input("Enter a number: "))
+steps = kaprekar_constant(user_input)
+print(f"Number of steps to reach Kaprekar constant: {steps}")
+
+
+# Answer: Enter a number: 2352
+#         Number of steps to reach Kaprekar constant: 3
