9f79937ea3f2d8583be030003823c35144d37cc9
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 13:49:57 +0600

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -8,3 +8,14 @@ if is_palindrome(user_input):
     print(f"'{user_input}' is a palindrome.")
 else:
     print(f"'{user_input}' is not a palindrome.")
+
+#------------------------------------------------------------
+
+def is_palindrome(s):
+    s = ''.join(filter(str.isalnum, s.lower()))
+    return s == s[::-1]
+
+# Test the function
+print(is_palindrome("A man, a plan, a canal: Panama"))  
+
+# Output: True
