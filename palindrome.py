86c15ef143d6e2a730c3d505a5f08befff21e524
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 13:39:59 +0600

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+def is_palindrome(s):
+    s = ''.join(filter(str.isalnum, s.lower()))
+    return s == s[::-1]
+
+# Prompt user for input
+user_input = input("Enter a string: ")
+if is_palindrome(user_input):
+    print(f"'{user_input}' is a palindrome.")
+else:
+    print(f"'{user_input}' is not a palindrome.")
