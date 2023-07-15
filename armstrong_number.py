57aaf0d4aca789221034fc437b62500dd5c70add
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 13:36:53 +0600

    Create armstrong_number.py

---
+++ armstrong_number.py
@@ -0,0 +1,16 @@
+def is_armstrong_number(n):
+    num_digits = len(str(n))
+    sum_of_powers = 0
+    temp = n
+    while temp > 0:
+        digit = temp % 10
+        sum_of_powers += digit ** num_digits
+        temp //= 10
+    return n == sum_of_powers
+
+# Prompt user for input
+user_input = int(input("Enter a number: "))
+if is_armstrong_number(user_input):
+    print(f"{user_input} is an Armstrong number.")
+else:
+    print(f"{user_input} is not an Armstrong number.")
